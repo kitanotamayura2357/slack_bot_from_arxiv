2eecfdcac3c291c937db82505732ab9a2d4a6c26
kitanotamayura <kitanotamayura2357@gmail.com>
2019-11-20 01:19:27 +0900

    make get_paper_from_arXiv

--- code/project/slack_post/get_paper_from_arxiv.py
+++ code/project/slack_post/get_paper_from_arxiv.py
@@ -12,22 +12,34 @@ paper_list = arxiv.query(query=f'au:"{args.author}"')
 
 if paper_list:
     author_name = args.author.replace(" ","_")
-    print(author_name)
-    new_dir_path = f'test_arXiv_download/{author_name}'
+    arxiv_path = 'downloaded_arxiv_paper'
+    if not os.path.isdir(arxiv_path):
+        os.mkdir(arxiv_path)
+    new_dir_path = f'downloaded_arxiv_paper/{author_name}'
     if not os.path.isdir(new_dir_path):
-        os.mkdir(new_dir_path)
+            os.mkdir(new_dir_path)
 
 def get_paper_name(paper):
-
-    authors_name = ','.join(paper['authors']).replace(' ', '.')
+    use_char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ%&():;? "
+    authors_name = ','.join(paper.get('authors')).replace(' ', '.')
     paper_id = paper.get('id').split('/')[-1]
-    paper_title = paper.get('title').replace('\n','').replace('  ',' ').replace(' ', '_').replace('/','-').replace(':','-').replace('"','~').replace(",","_")
-    return  paper_id + "." + paper_title + "(" +authors_name +")"
+    paper_title = paper.get('title').replace('\n','').replace('  ',' ').replace(' ', '_').replace('/','-').replace(':','-').replace('"','').replace(",","_").replace("---","").replace("__","_").replace("$","").replace("\\","").replace("*","").replace("?",'').replace('|','').replace('<','').replace('>','')
+    paper_title = f'{paper_title}'
+    paper_file_name = paper_id + "." + paper_title + "(" +authors_name +")"
+    aa = len('1611.05794v3.Sensitivity_of_quantum_walks_to_boundary_of_two-dimensional_lattices-_approaches_from_the_CGMV_method_and_topological_phases(Takako.Endo,Norio.Konno,Hideaki.Obuse,Etsuo.Segawa).pdf')
+    print("aa",aa)
+    if len(paper_file_name) > 180:
+        paper_file_name = paper_file_name = paper_id + "." + "(" +authors_name +")"
+    return paper_file_name
 
+    
+num = 0
 for paper in paper_list:
-    print(paper.get('title'))
+    num = num + 1
+    print("No.",num)
+    print("title",paper.get('title'))
     print(paper.get('id').split('/')[-1])
     print(paper.get('authors'))
-    #print("len",len(get_paper_name(paper)))
-    #arxiv.download(obj=paper, dirpath=f'test_arXiv_download/{author_name}', slugify=get_paper_name)
-    arxiv.download(obj=paper, dirpath=f'test_arXiv_download/{author_name}')
+    print("get_paper_name",get_paper_name(paper))
+    arxiv.download(obj=paper, dirpath=f'downloaded_arxiv_paper/{author_name}', slugify=get_paper_name)
+    #arxiv.download(obj=paper, dirpath=f'downloaded_arxiv_paper/{author_name}')
